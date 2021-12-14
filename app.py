016ea267ee6d5abc0c864e15212d64e51b0415eb
egoing <egoing@gmail.com>
2021-12-14 19:51:59 +0900

    기본 웹페이지가 출력되도록 변경

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Home"
+    return """
+    <html>
+    <body>
+            <h1><a href="/index.html">web</a></h1>
+            <ul>
+                <li><a href="/1.html">html</a></li>
+                <li><a href="/2.html">css</a></li>
+                <li><a href="/3.html">js</a></li>
+            </ul>
+            <h2>Welcome</h2>
+            Hello, WEB
+        </body>
+    </html>
+    """
 
 @app.route("/read/", defaults={'id': None})
 @app.route("/read/<id>/")
