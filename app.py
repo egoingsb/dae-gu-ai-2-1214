6f7d75f57064dc1b3809947a44390877e966ffd6
egoing <egoing@gmail.com>
2021-12-14 19:19:00 +0900

    라우팅

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import random
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Home"
+
+@app.route("/read/1/")
+def read():
+    return "Read"
+
+@app.route("/read/2/")
+def read2():
+    return "Read2"
+
+app.run(debug=True,host="0.0.0.0",port="8000")
